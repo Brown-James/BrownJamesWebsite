54205f69be49a606e612693a84f9870b2931af20
James Brown <jeb572@cs.bham.ac.uk>
2016-06-14 14:48:17 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$(".topBarItem").hover(function() {
+		$(this).css("background", '#455A64');
+	}, function() {
+		$(this).css("background", 'rgba(96,125,139,1)');
+	});
+});
